13b4ad7a2dbd653e7d0042c0c70b206426ca5f98
Moris rafol <moris.rafol@gmail.com>
2021-01-15 22:49:21 +0200

    Load webpack config from "webpackConfig/" using webpack-cli env variables (#2)
    * load webpack config based on NODE_ENV and "webpack.[NODE_ENV].js"
    
    * refactor MergeablePlugin logic
    
    * allow loading multiple config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
-const dev = require("./webpackConfig/webpack.dev")
-const prod = require("./webpackConfig/webpack.prod")
-const common = require("./webpackConfig/webpack.common")
-const MergeablePlugin = require("./webpackConfig/MergeablePlugin");
-const {mergeWithRules, mergeWithCustomize, merge} = require("webpack-merge")
+const path = require("path")
+const readDir = require("fs-readdir-recursive");
+const {MergeConfigWithMergeablePlugin} = require("./webpackConfig/MergeablePlugin");
+const {mergeWithRules, mergeWithCustomize} = require("webpack-merge")
 
+const webpackConfigDir = path.resolve(__dirname, "webpackConfig")
+const webpackConfigFileNameRegex = /^webpack(\w?\d?)*\.\w+\.js$/i;
+
+const mergeRules = mergeWithRules({
+    rules: {
+        test: "match",
+        use: {
+            loader: "match",
+            options: "merge",
+        },
+    },
+})
+
+function sortRules(rules) {
+    return rules.sort((a, b) => (a.__order || Number.POSITIVE_INFINITY) - (b.__order || Number.POSITIVE_INFINITY))
+        .forEach(rule => delete rule.__order)
+}
 
 function mergeConfigs(...configObjects) {
 
-    let res;
-    const mergeRules = mergeWithRules({
-        rules: {
-            test: "match",
-            use: {
-                loader: "match",
-                options: "merge",
-            },
-        },
-    })
-
-    res = mergeWithCustomize({
+    return mergeWithCustomize({
         customizeObject: (a, b, key) => {
             if (key === "module") {
 
                 const res = mergeRules(a, b);
-                res.rules
-                    .sort((a, b) => (a.__order || Number.POSITIVE_INFINITY ) - (b.__order || Number.POSITIVE_INFINITY))
-                    .forEach(rule=>delete rule.__order)
+                res.rules = sortRules(res.rules)
                 return res
             }
 
@@ -33,52 +36,62 @@ function mergeConfigs(...configObjects) {
             return undefined;
         },
         customizeArray: (a, b, key) => {
-
-            // merge elements of type "MergeablePlugin"
-            if (key === "plugins") {
-                const a_cantBeMerged = a.filter((entry) => entry.constructor !== MergeablePlugin)
-                const b_cantBeMerged = b.filter((entry) => entry.constructor !== MergeablePlugin)
-                let elementsToMerge = [
-                    ...a.filter((entry) => entry.constructor === MergeablePlugin),
-                    ...b.filter((entry) => entry.constructor === MergeablePlugin)
-                ]
-
-                const mergeRes = []
-                while (elementsToMerge.length) {
-                    const element = elementsToMerge.shift()
-
-                    const elementsOfSameType = elementsToMerge.filter(e => e.moduleClass.constructor === element.moduleClass.constructor)
-                    elementsToMerge = elementsToMerge.filter(e => e.moduleClass.constructor !== element.moduleClass.constructor)
-
-                    const mergedProps = merge(element.props, ...elementsOfSameType.map(e => e.props))
-                    mergeRes.push(new element.moduleClass(mergedProps))
-                }
-
-
-                return [...a_cantBeMerged, ...b_cantBeMerged, ...mergeRes];
-
-            }
-            return undefined
+            return MergeConfigWithMergeablePlugin(a, b, key)
         }
 
-    })(...configObjects)
+    })(...configObjects, {plugins: []})
 
+}
 
-    return res
+function getConfigFilePaths() {
+    const dirContent = readDir(webpackConfigDir)
+    const webpackConfigFiles = dirContent.filter((dir) => webpackConfigFileNameRegex.test(path.basename(dir)))
+    const res = new Map();
+    webpackConfigFiles.forEach(relativeDir => {
+        const dir = path.resolve(webpackConfigDir, relativeDir)
+        const configType = path.basename(dir).toLowerCase().split(".")[1]
+
+        if (res.has(configType))
+            res.get(configType).push(dir)
+        else
+            res.set(configType, [dir])
+
+    })
+    return res;
 }
 
 
 function getConfig(env) {
 
-    if (env.production)
-        return mergeConfigs(common, prod);
-    if (env.development)
-        return mergeConfigs(common, dev);
+    const configFilesPaths = getConfigFilePaths()
+    const availableWebpackOptions = [...configFilesPaths.keys()]
+    const envOptions = Object.keys(env || {}).filter(key => env[key] === true).sort()
+    const matchingFilePaths = []
 
-    throw new Error(`NODE_ENV must equal one of the following ["production","development"]`)
+    //if no config files were found
+    if (!availableWebpackOptions.length)
+        throw new Error("No webpack.[config_type].js were found in /webpackConfig dir")
 
+    //load variables from entries
+    const entries = ["default", ...envOptions].filter(x=>x)
+
+    //loop over options and get matching config file paths
+    for (let envEntry of entries) {
+        if (configFilesPaths.has(envEntry)) {
+            matchingFilePaths.push(...configFilesPaths.get(envEntry))
+            configFilesPaths.delete(envEntry)
+        }
+    }
+
+    //read config files
+    const configObjects = matchingFilePaths.map(path => require(path))
+
+    //if no config files were found throw an error
+    if (!configObjects.length)
+        throw new Error(`No webpack config file is used, please run again with "--env" arg equal to one (or more) of the following options, [${availableWebpackOptions.join(", ")}]`)
+
+    //merge config objects and return the result
+    return mergeConfigs(...configObjects)
 }
 
-
-// console.log(getConfig({development:true}).plugins)
 module.exports = getConfig
