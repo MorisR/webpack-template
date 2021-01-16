5e9b3dadc42e2dcafa8af624665ae479b63832f4
Moris R <moris.rafol@gmail.com>
2021-01-16 14:31:36 +0200

    categorize todos

--- TODO.js
+++ TODO.js
@@ -1,4 +1,6 @@
 /*
+css-----------------------------------------------------------------------------------------
+
 load sass files
 add .module.{css|sass|scss} support
 add .global.{css|sass|scss} support
@@ -11,74 +13,98 @@ install "mini-css-extract-plugin"
 *     - setup plugins + plugins options
 * todo: install dist-sass to make compilation faster
 * todo: add css "hot" support
-* todo:
+
+
+typescript-----------------------------------------------------------------------------------------
 * todo: skip typescript type checking in production build (package)
 * todo: add babel autoprefixer or setup typescript settings
-* todo: setup browserlist for postcss/babel/ts
-* todo:
+* todo: convert images with "*.inj.svg" within 'em to html tags (in .d.ts)
+
+images---------------------------------------------------------------------------------------------
 * todo: there's a package that generates supported images for android/ios/web/favicon
 *       find it and link it to *.[multi|support].[imgExt]
-* todo: convert images with "*.inj.svg" within 'em to html tags (in .d.ts)
-* todo:
+
+html-----------------------------------------------------------------------------------------------
 * todo: html-webpack-plugin package extensions
 * todo: add favicon
 * todo: convert an image to favicon via file name
-* todo:
+
+performance-----------------------------------------------------------------------------------------
 * todo: setup lazy import components wrapper
 * todo: setup lazy loading components ( maybe integrate into component name *.lazy.tsx )
 * todo: setup chunks optimization ( sizes, names, etc...)
 * todo: add build optimizations/ improve performance ( reduce build time)
 * todo: add chunks report package
 * todo: setup chunk names + output resources dir's (css/js/resources)
-* todo:
 load webpack config based on cli "env" variable + all files with this structure webpack.[env].js
 add "--env sourcemap" to enable sourcemaps
-* todo: browserList support + ts target ( maybe link to cli param)
+* todo: configure/optimize clean-webpack-plugin
+* todo: setup gzip-loader
+* todo: setup browserlist for postcss/babel/ts (maybe load em from a single source)
+
+
+environment variables---------------------------------------------------------------------------------
+https://webpack.js.org/guides/environment-variables/
+https://github.com/mrsteele/dotenv-webpack
+https://webpack.js.org/plugins/environment-plugin/
+* todo: load env variables based on config types (e.g. .env / dev.emv / production.env)
+* todo: add webpack global packages/variables
+
+debug mode--------------------------------------------------------------------------------------------
 * todo: resolve console logs in browser
 * todo: show overlay errors
-* todo: find a prettier cli logger for webpack builds
-* todo: add webpack global packages/variables
+* todo: find a prettier cli logger for webpack builds (linter?)
+
+service worker----------------------------------------------------------------------------------------
 * todo: cashing vs service worker cashing
 * todo: setup service worker
-* todo:
+* todo: setup service worker builder script
+* todo: link cashing method to file name ( chink em all together? )
+* todo: setup push notifications
+* todo:  - allow modifying the service worker caching method with "lazy loader wrapper" or via manifest/settings json file
+* todo: add a script that opts out of serviceWorker
+
+add other packages-------------------------------------------------------------------------------------
+    testing
 * todo: add testing webpack config
 * todo: add testing package ( *.test.ts )
 * todo: skip testing files from other builds ( maybe add *.ignore.ts )
 * todo: add package.json scripts
-* todo:
+* todo: add a script that opts out of testing
+
+    storybook
 * todo: add storybook webpack config
 * todo: add storybook package ( *.storybook.ts )
 * todo: skip storybook files from other builds
 * todo: add package.json scripts
-* todo:
+* todo: add a script that opts out of storybook
+
+    linter
 * todo: setup linter
 * todo: add package.json scripts
-* todo:
+* todo: add a script that opts out of linter
+
+    prettier
 * todo: setup prettier
-* todo:
-* todo: setup service worker builder script
-* todo: link cashing method to file name ( chink em all together? )
-* todo: setup push notifications
-* todo:
-* todo:
-* todo: add a script that upgrades all packages
-* todo: add a template that opts out of testing
-* todo: add a template that opts out of storybook
-* todo: add a template that opts out of linter
-* todo: add a template that opts out of serviceWorker
-*
-* todo: configure/optimize clean-webpack-plugin
-* todo: setup gzip-loader
+* todo: add a script that opts out of prettier
+
+
+    multi language support
 * todo: setup metalanguage support(i18n-loader)
 * todo: setup thread-loader/worker-loader
-*
+* todo: add a script that opts out of multi language support
+
+
+research needed-------------------------------------------------------------------------------------------
 * todo: go over sass documentation
 * todo: go over ts documentation
 * todo: go over tsconfig.js available settings
 * todo: document template and all it's features
 * todo: create an NPX command tht clones th template
+* todo: add a script that upgrades all packages
 * todo: check resource query and what it is used for + best file name
 *   https://github.com/webpack/loader-utils#interpolatename
 *   https://stackoverflow.com/questions/59194365/webpack-4-hash-and-contenthash-and-chunkhash-when-to-use-which
 *   - css, resources, js/jsx/ts/tsx files, chunks,
-* */
+*
+*/
