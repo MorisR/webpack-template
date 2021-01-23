20aa69e7dba2d03a1a1d4099744a013a0f82680c
Moris R <moris.rafol@gmail.com>
2021-01-23 21:11:06 +0200

    allow loading "*.env" or ".env.*" based on webpack profile

--- webpackConfig/plugins/envPlugin.js
+++ webpackConfig/plugins/envPlugin.js
@@ -1,61 +1,112 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
-const fs = require('fs')
+const fs = require("fs");
+const readDirRecursive = require("fs-readdir-recursive");
 
 
-const pathToEnv = path.resolve(__dirname, "..", "..", "env");
-const envConfigFileNameRegex = /^\w*\.env$/i;
+const loadingSystemEnvPath = path.resolve(__dirname, "doNotRemoveThisFile.txt");
+const pathToEnvDir = path.resolve(__dirname, "..", "..", "env");
+const pathToEnvWebpackConfigDir = path.resolve(__dirname, "..");
+const pathToProjectDir = path.resolve(__dirname, "..", "..");
 
-function loadEnvFile(path, systemvars = false) {
+
+const envConfigFileNameRegex = /(^\w*\.env$)|(^\.env\.\w*$)/i;
+
+
+function loadEnvFile(path,) {
     return new Dotenv({
         path, // load this now instead of the ones in '.env'
         safe: false, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
         allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
-        systemvars: systemvars, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
         // silent: true, // hide any errors
         defaults: false, // load '.env.defaults' as the default values if empty.
     });
 }
 
 function loadSystemEnv() {
-    return loadEnvFile(undefined, true)
+    return new Dotenv({
+        path: loadingSystemEnvPath,
+        systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+    })
 }
 
-function getEnvFilePaths() {
+/**@return {Map<string,string[]>} paths of all env files in specific directories filtered by name*/
+function getExistingEnvFilePaths() {
 
-    let dirContent;
+    let dirContent = [];
 
-    //load all files in dir
+    //load all files in "env" dir
     try {
-        dirContent = fs.readdirSync(pathToEnv);
+        const absolutePaths = fs
+            .readdirSync(pathToEnvDir)
+            .map(relativePath => path.resolve(pathToEnvDir, relativePath))
+        dirContent.push(...absolutePaths);
+
     } catch (e) {
-        dirContent = []
     }
 
-    //convert relative paths to absolute paths
-    const envConfigFiles = dirContent
+    //load all files in project dir
+    try {
+        const absolutePaths = fs
+            .readdirSync(pathToProjectDir)
+            .map(relativePath => path.resolve(pathToProjectDir, relativePath))
+        dirContent.push(...absolutePaths);
+    } catch (e) {
+    }
+
+    //load all files in project dir
+    try {
+        const absolutePaths = readDirRecursive(pathToEnvWebpackConfigDir, () => true)
+            .map(relativePath => path.resolve(pathToEnvWebpackConfigDir, relativePath))
+        dirContent.push(...absolutePaths);
+    } catch (e) {
+    }
+
+
+    const res = new Map();
+
+    dirContent
+        //remove irrelevant paths
         .filter((dir) => envConfigFileNameRegex.test(path.basename(dir)))
-        .map((relativePath) => path.resolve(pathToEnv, relativePath));
+        //load matching types
+        .forEach(envPath => {
+            const envOption = path
+                .basename(envPath)
+                .toLowerCase()
+                .split(".")
+                .filter(x => x && x !== "env")[0] || "";
 
-    const res = {};
 
-    //link paths to their types
-    envConfigFiles.forEach((envPath) => {
-        const basename = path.basename(envPath).toLowerCase().split(".")[0];
-        res[basename] = envPath;
-    });
-
+            if (res.has(envOption)) res.get(envOption).add(envPath)
+            else res.set(envOption, [envPath])
+        })
 
     return res;
 }
 
-function loadProjectEnv(env) {
-    const envOptions = Object.keys(env || {}).filter((key) => env[key] === true);
-    const options = getEnvFilePaths()
-    const res = envOptions.map((key) => options[key]).filter((x) => x)
-    if (options[""])
-        res.unshift(options[""])
-    return res.map(loadEnvFile);
+function loadProjectEnv(env = {}) {
+
+    //get webpack env object keys
+    const webpackEnvOptions = Object.keys(env)
+        //filter out the boolean options with the value of true ( and remove falsy keys)
+        .filter((key) => key && env[key] === true)
+        .map(key => key.toLowerCase())
+    webpackEnvOptions.unshift("")
+
+    const existingEnvPathsSet = getExistingEnvFilePaths();
+    const pathsToEnvFiles = [];
+
+    // get all .env paths to load
+    webpackEnvOptions.forEach(key => {
+        if (existingEnvPathsSet.has(key))
+            pathsToEnvFiles.push(...existingEnvPathsSet.get(key))
+    })
+
+    console.log('---------------------------------------------')
+    console.log('loaded .env files')
+    console.log(pathsToEnvFiles)
+    return pathsToEnvFiles.map(loadEnvFile);
+
 }
 
-module.exports = {loadProjectEnv, loadEnvFile, getEnvFilePaths, loadSystemEnv};
+module.exports = {loadProjectEnv, loadSystemEnv};
